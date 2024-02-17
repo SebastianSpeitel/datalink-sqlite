218331381a9c4723f4d061a41d44870e5e57d64b
Sebastian Speitel <sebastian.speitel@outlook.de>
2024-02-17 15:23:29 +0100

    feat: add Storable trait to call store on any Unique

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,13 @@
 pub mod database;
 pub mod error;
 mod query;
+pub mod storable;
 pub mod storeddata;
 
 pub use rusqlite;
 
 pub mod prelude {
     pub use crate::database::Database;
+    pub use crate::storable::Storable;
     pub use crate::storeddata::StoredData;
 }
---
+++ src/storable.rs
@@ -0,0 +1,38 @@
+use datalink::prelude::*;
+
+use crate::{database::Database, error::Result, storeddata::StoredData};
+
+pub trait Storable {
+    fn store(&self, db: &Database) -> Result<StoredData>;
+
+    #[inline]
+    fn into_stored(self, db: &Database) -> Result<StoredData>
+    where
+        Self: Sized,
+    {
+        self.store(db)
+    }
+}
+
+impl<D: Unique + ?Sized> Storable for D {
+    #[inline]
+    fn store(&self, db: &Database) -> Result<StoredData> {
+        db.store(self)
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::database::Database;
+    use datalink::data::DataExt;
+
+    #[test]
+    fn test_storable() {
+        let db = Database::open_in_memory().unwrap();
+        db.init().unwrap();
+        let data = true.into_unique_random();
+        let stored = data.store(&db).unwrap();
+        assert_eq!(stored.as_bool().unwrap(), true);
+    }
+}
